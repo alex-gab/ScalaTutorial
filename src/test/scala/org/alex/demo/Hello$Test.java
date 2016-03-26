2e2650ef4fb2bae446abf3c11efe5a335dfd9d4e
Alex Dragoi <dragoi.alex@gmail.com>
2016-03-26 08:12:58 +0200

    [#1] create test directory

---
+++ src/test/scala/org/alex/demo/Hello$Test.java
@@ -0,0 +1,13 @@
+package org.alex.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Hello$Test {
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
